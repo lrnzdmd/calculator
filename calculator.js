7cabbcab6d4467ff281126d5c38c93561c15c2c2
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-11 05:13:22 +0200

    basic functionality working, still a lot to do

--- calculator.js
+++ calculator.js
@@ -1,16 +1,108 @@
+let display;
+let displayValue = "0";
+let firstNumber = "0";
+let secondNumber = "";
+let operator = "";
+let displaySwitch = false;
+
+document.addEventListener("DOMContentLoaded", function() {
+    display = document.querySelector(".display");
+    let buttons = document.querySelectorAll("button");
+    for (let i=0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains("number")){
+        buttons[i].addEventListener("click", function() {
+            displayNumber(buttons[i].value);
+        });
+        }
+        else if (buttons[i].classList.contains("delete")){
+            buttons[i].addEventListener("click", function() {
+                clearDisplay();
+            });
+        }
+        else if (buttons[i].classList.contains("operator")){
+            buttons[i].addEventListener("click", function(){
+                setOperator(buttons[i].value);
+            });
+        }
+        else if (buttons[i].classList.contains("equals")){
+            buttons[i].addEventListener("click", function(){
+                equalsOperate();
+            });
+        }
+    }
+
+});
+
+function equalsOperate(){
+    if (operator) {secondNumber = displayValue;}
+    if (secondNumber !== "" && operator){
+    displayValue = operate(firstNumber, secondNumber, operator);
+    firstNumber = displayValue;
+    display.innerHTML = displayValue;
+    secondNumber = "";
+    operator = "";
+    }
+
+}
+
+function setOperator(oper){
+    if (operator) {
+        secondNumber = displayValue;
+    }
+
+
+
+    if (secondNumber !== ""){
+        firstNumber = operate(firstNumber,secondNumber,operator);
+        secondNumber = "";
+        operator = "";
+        displaySwitch = true;
+        displayValue = firstNumber;
+        display.innerHTML = displayValue;
+    }
+    firstNumber = displayValue;
+    operator = oper;
+    displaySwitch = true;
+}
+
+function clearDisplay() {
+    display.innerHTML = "0";
+    displayValue = display.innerHTML;
+}
+
+function displayNumber(number) {
+
+    if (displayValue === "0" || displaySwitch)
+        {
+            display.innerHTML = "";
+            displayValue = "";
+            displayValue += number;
+            display.innerHTML = displayValue;
+            displaySwitch = false;
+            
+        }
+        else {
+
+            if (displayValue.length <= 10) {displayValue += number;}
+            display.innerHTML = displayValue;
+        }
+    
+    }
+
+
 function add(firstNumber, secondNumber) {
     let result = parseFloat(firstNumber) + parseFloat(secondNumber);
-    return result; 
+    return result.toFixed(2); 
 }
 
 function subtract(firstNumber, secondNumber) {
     let result = parseFloat(firstNumber) - parseFloat(secondNumber);
-    return result; 
+    return result.toFixed(2); 
 }
 
 function multiply(firstNumber, secondNumber) {
     let result = parseFloat(firstNumber) * parseFloat(secondNumber);
-    return result; 
+    return result.toFixed(2); 
 }
 
 function divide(firstNumber, secondNumber) {
@@ -18,7 +110,7 @@ function divide(firstNumber, secondNumber) {
         let result = "5318008"
     }
     let result = parseFloat(firstNumber) / parseFloat(secondNumber);
-    return result; 
+    return result.toFixed(2); 
 }
 
 function operate(firstNumber, secondNumber, operator) {
@@ -38,6 +130,6 @@ function operate(firstNumber, secondNumber, operator) {
             break;
     }
 
-    return result;
+    return result.toString().slice(-11);
 
 }
