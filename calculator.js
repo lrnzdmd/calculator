b9d7126a49a8d8823d3f0e4fd1b8ffca3c8bdedc
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-11 02:18:34 +0200

    base functions for the calculator done, i can smell the problems coming

--- calculator.js
+++ calculator.js
@@ -1,1 +1,43 @@
-console.log("hello world");
+function add(firstNumber, secondNumber) {
+    let result = parseFloat(firstNumber) + parseFloat(secondNumber);
+    return result; 
+}
+
+function subtract(firstNumber, secondNumber) {
+    let result = parseFloat(firstNumber) - parseFloat(secondNumber);
+    return result; 
+}
+
+function multiply(firstNumber, secondNumber) {
+    let result = parseFloat(firstNumber) * parseFloat(secondNumber);
+    return result; 
+}
+
+function divide(firstNumber, secondNumber) {
+    if (secondNumber === 0) { 
+        let result = "5318008"
+    }
+    let result = parseFloat(firstNumber) / parseFloat(secondNumber);
+    return result; 
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(firstNumber,secondNumber);
+            break;
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "*": 
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+    }
+
+    return result;
+
+}
